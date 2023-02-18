f3103038f0e47141251bb90023dc1772d3f77a96
mdshahed <mdshahedrahman14@gmail.com>
2023-02-18 18:58:05 +0600

    first card javascript

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById('card1-btn').addEventListener('click',function(){
+    // console.log('clicked')
+    const firstCardInput1String=document.getElementById('card1-input1').value
+    const firstCardInput1 =parseFloat(firstCardInput1String);
+    // console.log(typeof firstCardInput1)
+
+    const firstCardInput2String=document.getElementById('card1-input2').value
+    const firstCardInput2 =parseFloat(firstCardInput2String);
+    // console.log(typeof firstCardInput2)
+})
